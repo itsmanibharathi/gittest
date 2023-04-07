a8c8a458facdce94c759a06a2a80150042d48286
manibharthi <manibharthi@gmail.com>
2023-04-07 17:51:46 +0530

    add/get/save/controlls

--- index.js
+++ index.js
@@ -3,7 +3,7 @@ const gitControllers = require('./gitControllers')
 const Fs = require('fs')  
 
 const app = express();
-app.listen(5000, (err) => {
+app.listen(80, (err) => {
   if (err) {
     console.log(err);
   } else {
@@ -23,7 +23,22 @@ app.get('/', (req, res) => {
         
         res.status(200).json({mag:error})
     }
-  })
+})
 
+app.get('/ls', (req, res) => {
+const { exec } = require("child_process");
+
+exec("ls codestack -la", (error, stdout, stderr) => {
+    if (error) {
+        console.log(`error: ${error.message}`);
+        return;
+    }
+    if (stderr) {
+        console.log(`stderr: ${stderr}`);
+        return;
+    }
+    console.log(`stdout: ${stdout}`);
+});
+});
 // git.push();
 // git.commit("simple-git init commit ");cls
