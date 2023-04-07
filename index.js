65f71bb8331e6252810a6b084e038d757c1a7fb2
manibharthi <manibharthi@gmail.com>
2023-04-07 17:41:42 +0530

    add/get/save/controlls

--- index.js
+++ index.js
@@ -1,6 +1,29 @@
+const express = require("express");
 const gitControllers = require('./gitControllers')
-const git=new gitControllers('codestack/')
-git.clone('https://github.com/itsmanibharathi/React-Testing')
-console.log('data');
+const Fs = require('fs')  
+
+const app = express();
+app.listen(5000, (err) => {
+  if (err) {
+    console.log(err);
+  } else {
+    console.log("Server Started Successfully.");
+  }
+});
+app.get('/', (req, res) => {
+    try {
+        const git=new gitControllers('codestack/')
+        if(Fs.existsSync('./codestack/React-Testing'))
+        res.status(200).json({mag :"al"})
+        else{
+            git.clone('https://github.com/itsmanibharathi/React-Testing')
+            res.status(200).json({mag :"ok"})
+        }
+    } catch (error) {
+        
+        res.status(200).json({mag:error})
+    }
+  })
+
 // git.push();
-// git.commit("simple-git init commit ");
+// git.commit("simple-git init commit ");cls
