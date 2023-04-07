e6dc7617c64cd39e3c72b9fe16f0e962011f7d53
Mani <manibharathidct@gmail.com>
2023-04-07 22:37:51 +0530

    add/gitcontroll

--- codestack/src/App.test.js
+++
@@ -1,8 +0,0 @@
-import { render, screen } from '@testing-library/react';
-import App from './App';
-
-test('check itsmanibharathi is present ', () => {
-  render(<App />);
-  const linkElement = screen.getByText(/By itsmanibharathi/i);
-  expect(linkElement).toBeInTheDocument();
-});
